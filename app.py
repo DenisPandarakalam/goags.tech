736b62aebad5d3cae3564fbfdeb86bf2fa95e6dd
Denis Pandarakalam <32884052+DenisPandarakalam@users.noreply.github.com>
2022-04-16 20:09:11 -0700

    Goags.tech
    HackDavis2022

---
+++ app.py
@@ -0,0 +1,118 @@
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
+
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from functools import wraps
+# from flask.ext.sqlalchemy import SQLAlchemy
+import logging
+from logging import Formatter, FileHandler
+from forms import *
+import os
+
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
+app = Flask(__name__)
+app.config.from_object('config')
+#db = SQLAlchemy(app)
+
+# Automatically tear down SQLAlchemy.
+'''
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
+'''
+
+# Login required decorator.
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'loggedIn' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+#----------------------------------------------------------------------------#
+# Controllers.
+#----------------------------------------------------------------------------#
+
+
+session = {
+    "profile": {
+        "name": "Denis"
+    },
+    "loggedIn": "True"
+}
+
+
+@app.route('/')
+def home():
+    return redirect(url_for('calendar'))
+
+
+@app.route('/calendar')
+def calendar():
+    return render_template('pages/placeholder.home.html', session=session)
+
+@app.route('/profile')
+def profile():
+    return render_template('pages/placeholder.profile.html', session=session)
+
+@app.route('/login')
+def login():
+    form = LoginForm(request.form)
+    return render_template('forms/login.html', form=form)
+
+
+@app.route('/register')
+def register():
+    form = RegisterForm(request.form)
+    return render_template('forms/register.html', form=form)
+
+
+@app.route('/forgot')
+def forgot():
+    form = ForgotForm(request.form)
+    return render_template('forms/forgot.html', form=form)
+
+# Error handlers.
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    #db_session.rollback()
+    return render_template('errors/500.html'), 500
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+if not app.debug:
+    file_handler = FileHandler('error.log')
+    file_handler.setFormatter(
+        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    )
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('errors')
+
+#----------------------------------------------------------------------------#
+# Launch.
+#----------------------------------------------------------------------------#
+
+# Default port:
+'''
+if __name__ == '__main__':
+    app.run()
+'''
+
+# Or specify port manually:
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5023))
+    app.run(host='0.0.0.0', port=port)
